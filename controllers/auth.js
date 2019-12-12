6c7caeefb53f3a84383f008f4296726576f22607
kengru <kengrullon@gmail.com>
2019-12-12 14:32:24 -0400

    Fixed signup duplication and sending correct token.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,23 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { validationResult } from "express-validator";
 
 import User from "../models/user";
 
 const signUp = async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error = new Error("Validation failed, entered data is incorrect.");
-    error.statusCode = 422;
-    error.data = errors.array();
-    next(error);
-  }
-
-  const email = req.body.email;
-  const username = req.body.username;
-  const password = req.body.password;
-  
   try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error = new Error("Validation failed, entered data is incorrect.");
+      error.statusCode = 422;
+      error.data = errors.array();
+      throw error;
+    }
+
+    const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
+
     const hashedPw = await bcrypt.hash(password, 12);
     const user = new User({
       email: email,
@@ -34,33 +35,41 @@ const signUp = async (req, res, next) => {
     }
     next(err);
   }
-}
+};
 
 const logIn = async (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  
+
   try {
     const user = await User.findOne({ email: email });
     if (!user) {
       const error = new Error("A user with this email could not be found.");
       error.statusCode = 401;
-      next(error);
+      throw error;
     }
     const check = await bcrypt.compare(password, user.password);
     if (!check) {
       const error = new Error("Wrong password!");
       error.statusCode = 401;
-      next(error);
+      throw error;
     }
-    
+    const token = jwt.sign(
+      { email: user.email, userId: user._id.toString() },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({
+      token: token,
+      userId: user._id.toString()
+    });
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
     }
     next(err);
   }
-}
+};
 
 export default {
   signUp: signUp,
