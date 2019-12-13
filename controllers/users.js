ecbeaae856d2ac9cc2cc66d21b811924630f7fe9
kengru <kengrullon@gmail.com>
2019-12-13 11:10:29 -0400

    Basi user manga retreieved. Add and remove.

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 import _ from "lodash";
 
 import User from "../models/user";
+import Manga from "../models/manga";
 
 const getUser = async (req, res, next) => {
   const userId = req.userId;
@@ -18,13 +19,79 @@ const getUser = async (req, res, next) => {
   }
 };
 
-const getMangas = async (req, res, next) => {};
-
-const addManga = async (req, res, next) => {
-  const mangaId = req.params.mangaId;
+const getMangas = async (req, res, next) => {
+  const userId = req.userId;
+  try {
+    const user = await User.findById(userId);
+    res.status(200).json({
+      message: "Mangas retrieved.",
+      mangas: user.mangas
+    });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
-const removeManga = async (req, res, next) => {};
+const addManga = async (req, res, next) => {
+  const userId = req.userId;
+  const mangaId = req.params.mangaId;
+  try {
+    const user = await User.findById(userId);
+    if (user.mangas.includes(mangaId)) {
+      const err = new Error("This manga already exists on the user.");
+      err.statusCode = 400;
+      throw err;
+    }
+    const manga = await Manga.findById(mangaId);
+    if (!manga) {
+      const err = new Error("The ID provided does not exists.");
+      err.statusCode = 404;
+      throw err;
+    }
+    user.mangas.push(manga);
+    user.save();
+    res.status(201).json({
+      message: "Manga added."
+    });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
+const removeManga = async (req, res, next) => {
+  const userId = req.userId;
+  const mangaId = req.params.mangaId;
+  try {
+    const user = await User.findById(userId);
+    if (!user.mangas.includes(mangaId)) {
+      const err = new Error("This manga is not assigned to the user.");
+      err.statusCode = 400;
+      throw err;
+    }
+    const manga = await Manga.findById(mangaId);
+    if (!manga) {
+      const err = new Error("The ID provided does not exists.");
+      err.statusCode = 404;
+      throw err;
+    }
+    user.mangas.pull(manga);
+    user.save();
+    res.status(201).json({
+      message: "Manga removed."
+    });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
 
 export default {
   getUser: getUser,
